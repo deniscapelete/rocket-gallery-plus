59416c3a5ac5c4596f4dc5d5d62b7f73532ed3a1
Denis Capelete <capelete.denis@gmail.com>
2025-10-11 19:28:24 -0300

    Mock da lista de fotos

--- src/contexts/photos/components/photo-widget.tsx
+++ src/contexts/photos/components/photo-widget.tsx
@@ -13,15 +13,15 @@ interface PhotoWidgetProps {
 
 export default function PhotoWidget({ photo, loading }: PhotoWidgetProps) {
   return (
-    <div className="flex flex-col gap-4 ">
+    <div className="flex flex-col gap-4 max-w-[10.875rem]">
       {!loading ? (
         <ImagePreview
           src={`/images/${photo.imageId}`}
           title={photo.title}
-          imageClassName="w-[13.5625rem] h-[13.5625rem] rounded-lg"
+          imageClassName="w-[10.875rem] h-[10.875rem] rounded-lg"
         />
       ) : (
-        <Skeleton className="w-[13.5625rem] h-[13.5625rem] rounded-lg" />
+        <Skeleton className="w-[10.875rem] h-[10.875rem] rounded-lg" />
       )}
 
       <div className="flex flex-col gap-2">
@@ -36,13 +36,13 @@ export default function PhotoWidget({ photo, loading }: PhotoWidgetProps) {
         <div className="flex gap-1 min-h-[1.375rem]">
           {!loading ? (
             <>
-              {photo.albums.slice(0, 2).map((album) => (
+              {photo.albums.slice(0, 1).map((album) => (
                 <Badge className="truncate" size="xs" key={album.id}>
                   {album.title}
                 </Badge>
               ))}
-              {photo.albums.length > 2 && (
-                <Badge size="xs">+{photo.albums.length - 2}</Badge>
+              {photo.albums.length > 1 && (
+                <Badge size="xs">+{photo.albums.length - 1}</Badge>
               )}
             </>
           ) : (
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,52 +1,31 @@
 import Container from "../components/container"
-import PhotoWidget from "../contexts/photos/components/photo-widget"
-import type { Photo } from "../contexts/photos/models/photo"
+import PhotosList from "../contexts/photos/components/photos-list"
 
 export default function PageHome() {
   return (
     <Container>
-      <div className="grid grid-cols-4 gap-9">
-        <PhotoWidget
-          photo={{
-            id: '123',
-            title: 'Olá mundo',
-            imageId: "portrait-tower.png",
-            albums: [
-              { id: '321', title: "album 1" },
-              { id: '322', title: "album 2" },
-              { id: '323', title: "album 3" }
-            ]
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: '123',
-            title: 'Olá mundo',
-            imageId: "portrait-tower.png",
-            albums: [
-              { id: '321', title: "album 1" },
-              { id: '322', title: "album 2" },
-              { id: '323', title: "album 3" }
-            ]
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: '123',
-            title: 'Olá mundo',
-            imageId: "portrait-tower.png",
-            albums: [
-              { id: '321', title: "album 1" },
-              { id: '322', title: "album 2" },
-              { id: '323', title: "album 3" }
-            ]
-          }}
-        />
-
-        <PhotoWidget
-          photo={{} as Photo} loading
-        />
-      </div>
+      <PhotosList photos={[
+        {
+          id: '123',
+          title: 'Olá mundo',
+          imageId: "portrait-tower.png",
+          albums: [
+            { id: '321', title: "album 1" },
+            { id: '322', title: "album 2" },
+            { id: '323', title: "album 3" }
+          ]
+        },
+        {
+          id: '122',
+          title: 'Olá mundo',
+          imageId: "portrait-tower.png",
+          albums: [
+            { id: '321', title: "album 1" },
+            { id: '322', title: "album 2" },
+            { id: '323', title: "album 3" }
+          ]
+        }
+      ]} />
     </Container>
   )
 }
