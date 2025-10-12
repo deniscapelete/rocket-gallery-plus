2d4d50de8fa5ceffe76946828a9827d581687ed8
Denis Capelete <capelete.denis@gmail.com>
2025-10-12 19:25:57 -0300

    Mock da página de detalhes da foto - Header da página

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,13 +1,37 @@
 import { useParams } from "react-router";
 import Text from "../components/text";
+import Container from "../components/container";
+import type { Photo } from "../contexts/photos/models/photo";
+import Skeleton from "../components/skeleton";
+import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 
 export default function PagePhotoDetails() {
   const { id } = useParams();
+  // Apenas para teste do mock
+  const isLoadingPhoto = false;
+  const photo = {
+    id: '123',
+    title: 'Olá mundo',
+    imageId: "portrait-tower.png",
+    albums: [
+      { id: '321', title: "album 1" },
+      { id: '322', title: "album 2" },
+      { id: '323', title: "album 3" }
+    ]
+  }
 
   return (
-    <>
-      <Text variant="heading-medium">Página detalhe da foto</Text>
-      <Text variant="heading-medium">Id: {id}</Text>
-    </>
+    <Container>
+      <header className="flex items-center justify-between gap-8 mb-8">
+        {!isLoadingPhoto ? (
+          <Text variant="heading-large"> {photo?.title} </Text>
+        ) : (
+          <Skeleton className="w-48 h-8" />
+        )}
+
+
+        <PhotosNavigator loading={isLoadingPhoto} />
+      </header>
+    </Container>
   )
 }
