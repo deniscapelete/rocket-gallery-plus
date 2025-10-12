3ba5b6376b37f5187db8e09e35ec4463b31fef48
Denis Capelete <capelete.denis@gmail.com>
2025-10-12 19:05:33 -0300

    Mock do filtro por álbuns

---
+++ src/contexts/albums/components/albums-filter.tsx
@@ -0,0 +1,45 @@
+import Button from "../../../components/button";
+import Skeleton from "../../../components/skeleton";
+import Text from "../../../components/text";
+import type { Album } from "../models/album";
+import cx from "classnames"
+
+interface AlbumsFilterProps extends React.ComponentProps<"div"> {
+  albums: Album[],
+  loading?: boolean
+}
+
+export default function AlbumsFilter({ albums, loading, className, ...props }: AlbumsFilterProps) {
+
+  return (
+    <div
+      className={cx("flex items-center gap-3.5 overflow-x-auto", className)}
+      {...props}
+    >
+      <Text variant="heading-small">Álbuns</Text>
+      <div className="flex gap-3">
+
+        {!loading && (
+          <>
+            <Button size="sm" className="cursor-pointer" variant="primary">
+              Todos
+            </Button>
+            {albums.map(album =>
+              <Button key={album.id} size="sm" className="cursor-pointer" variant="ghost">
+                {album.title}
+              </Button>
+            )}
+          </>
+        )}
+
+        {loading &&
+          Array.from({ length: 5 }).map((_, index) =>
+            <Skeleton
+              key={`album-button-loading-${index}`}
+              className="w-28 h-7"
+            />
+          )}
+      </div>
+    </div>
+  )
+}
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,20 @@
 import Container from "../components/container"
+import AlbumsFilter from "../contexts/albums/components/albums-filter"
 import PhotosList from "../contexts/photos/components/photos-list"
 
 export default function PageHome() {
   return (
     <Container>
+      <AlbumsFilter
+        albums={[
+          { id: '321', title: "album 1" },
+          { id: '322', title: "album 2" },
+          { id: '323', title: "album 3" }
+        ]}
+        className="mb-9"
+        loading
+      />
+
       <PhotosList photos={[
         {
           id: '123',
