6a23be58de7231c20a0eee099155bea25b95ecae
Denis Capelete <capelete.denis@gmail.com>
2025-10-13 21:03:17 -0300

    Mock da página de detalhes da foto - Lista de álbuns selecionáveis

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -5,6 +5,8 @@ import Skeleton from "../components/skeleton";
 import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 import ImagePreview from "../components/image-preview";
 import Button from "../components/button";
+import AlbumsListSelectable from "../contexts/albums/components/albums-list-selectable";
+import type { Photo } from "../contexts/photos/models/photo";
 
 export default function PagePhotoDetails() {
   const { id } = useParams();
@@ -20,12 +22,17 @@ export default function PagePhotoDetails() {
       { id: '323', title: "album 3" }
     ]
   }
+  const albums = [
+    { id: '321', title: "album 1" },
+    { id: '322', title: "album 2" },
+    { id: '323', title: "album 3" }
+  ]
 
   return (
     <Container>
       <header className="flex items-center justify-between gap-8 mb-8">
         {!isLoadingPhoto ? (
-          <Text variant="heading-large"> {photo?.title} </Text>
+          <Text as="h2" variant="heading-large"> {photo?.title} </Text>
         ) : (
           <Skeleton className="w-48 h-8" />
         )}
@@ -34,7 +41,7 @@ export default function PagePhotoDetails() {
         <PhotosNavigator loading={isLoadingPhoto} />
       </header>
 
-      <div className="grid grid-cols-[21rem] gap-24">
+      <div className="grid grid-cols-[21rem_1fr] gap-24">
         <div className="space-y-3">
           {!isLoadingPhoto ?
             <ImagePreview
@@ -51,6 +58,14 @@ export default function PagePhotoDetails() {
             :
             <Skeleton className="w-20 h-10" />
           }
+        </div>
+
+        <div className="py-3">
+          <Text as="h3" variant="heading-medium" className="mb-6">
+            Albuns
+          </Text>
+
+          <AlbumsListSelectable albums={albums} photo={photo} loading={isLoadingPhoto} />
         </div>
       </div>
     </Container>
