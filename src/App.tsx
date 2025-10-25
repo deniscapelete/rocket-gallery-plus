739fd84c0c76528d83e158b9e8ce3f79fbc3a9cb
Denis Capelete <capelete.denis@gmail.com>
2025-10-25 20:54:59 -0300

    Configurando React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import PageComponents from "./pages/page-components";
 import LayoutMain from "./pages/layout-main";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import PagePhotoDetails from "./pages/page-photo-details";
 import PageHome from "./pages/page-home";
 
+const queryClient = new QueryClient();
+
 export default function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route element={<LayoutMain />}>
-					<Route path="/" element={<PageHome />} />
-					<Route path="/fotos/:id" element={<PagePhotoDetails />} />
-					<Route path="/componentes" element={<PageComponents />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<LayoutMain />}>
+						<Route path="/" element={<PageHome />} />
+						<Route path="/fotos/:id" element={<PagePhotoDetails />} />
+						<Route path="/componentes" element={<PageComponents />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
 	);
 }
