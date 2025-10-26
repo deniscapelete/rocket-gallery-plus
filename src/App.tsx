d3514fe35f9f682fc19a90b4d56a56d8b4f14fd9
Denis Capelete <capelete.denis@gmail.com>
2025-10-26 13:24:13 -0300

    Conhecendo o NUQS

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import PageComponents from "./pages/page-components";
 import LayoutMain from "./pages/layout-main";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { NuqsAdapter } from "nuqs/adapters/react-router/v7"
 
 import PagePhotoDetails from "./pages/page-photo-details";
 import PageHome from "./pages/page-home";
@@ -11,15 +12,17 @@ const queryClient = new QueryClient();
 export default function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
-				<Routes>
-					<Route element={<LayoutMain />}>
-						<Route path="/" element={<PageHome />} />
-						<Route path="/fotos/:id" element={<PagePhotoDetails />} />
-						<Route path="/componentes" element={<PageComponents />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
-		</QueryClientProvider>
+			<NuqsAdapter>
+				<BrowserRouter>
+					<Routes>
+						<Route element={<LayoutMain />}>
+							<Route path="/" element={<PageHome />} />
+							<Route path="/fotos/:id" element={<PagePhotoDetails />} />
+							<Route path="/componentes" element={<PageComponents />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			</NuqsAdapter>
+		</QueryClientProvider >
 	);
 }
